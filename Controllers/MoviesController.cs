81b4107b33497f47ec435e2d4dd143972a1f3d78
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-16 09:56:50 -0600

    Init MovieWebApi github

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWebAPI.Data;
+using MovieWebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            _context.movies.Add(movie);
+            _context.SaveChanges();
+            return StatusCode(201,movie);
+        }
+
+        // PUT api/<MoviesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieWebAPI.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MovieWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Movie> movies { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
